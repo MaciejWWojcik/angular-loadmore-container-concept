2ac5c13318b4c2dfaa6790cbebf2b047edbe5ef6
MaciejWWojcik <maciejwwojcik@gmail.com>
2020-08-05 09:53:20 +0200

    chunked-data moved to separate class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,7 @@
 import { Component, VERSION, OnInit } from "@angular/core";
 import { NamesService } from "./names.service";
+import { ChunkedData } from "./chunked-data";
 
-class ChunkedData<T> {
-  private data: T[][] = [];
-
-  get chunks(): T[][] {
-    return this.data;
-  }
-
-  // calc amount of chunks
-  get length(): number {
-    return this.data.length;
-  }
-
-  // calc amount of all items in all chunks
-  get totalLength(): number {
-    return this.data.reduce((acc, curr) => acc + curr.length, 0);
-  }
-
-  set(chunk: T[]) {
-    this.data = [chunk];
-  }
-
-  push(chunk: T[]) {
-    this.data.push(chunk);
-  }
-}
 
 @Component({
   selector: "my-app",
